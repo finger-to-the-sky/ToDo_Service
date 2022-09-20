b7d1e237d266bfff6e713e5114e0971fed4bb48f
zagmak <mzagrebam@gmail.com>
2022-09-20 23:34:18 +0300

    add Shemas and graphene_django

--- ToDo_Service/urls.py
+++ ToDo_Service/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg .views import get_schema_view
 from drf_yasg import openapi
-
+from graphene_django.views import GraphQLView
 
 schema_view = get_schema_view(openapi.Info(
         title="Library",
@@ -46,13 +46,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    re_path(r'^api/(?P<version>\d\.\d)/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
+#     re_path(r'^api/(?P<version>\d\.\d)/', include(router.urls)),
+#     path('api-token-auth/', views.obtain_auth_token),
+#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#
+#     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
